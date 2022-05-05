b562fc1490b7ab1d89b89dd0e589581dd7b66fdf
CHENG CHIEN <hcc780921@gmail.com>
2022-05-05 16:02:38 -0700

    registerRoute added

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -28,6 +28,15 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
 registerRoute(
+  // check if req destination that includes style, script or worker
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination), 
+  new StaleWhileRevalidate({
+    cacheName: 'asset-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
 
-  
 );
